5228db453071f4b1a2de4158b4ddaa3046c13351
austinpagotto <austinpagotto@gmail.com>
2020-08-01 17:34:29 -0400

    initial commit

---
+++ src/reactFace/BackgroundCircle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function BackgroundCircle({radius,strokeWidth}){
+    return(
+        <circle
+        r={radius}
+        fill="yellow"
+        stroke="black"
+        strokeWidth={strokeWidth}
+      ></circle>
+    )
+}
+
+export default BackgroundCircle
---
+++ src/reactFace/FaceContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FaceContainer({ width,height,centerX,centerY,children }) {
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX},${centerY})`}>
+          {children}
+      </g>
+    </svg>
+  );
+}
+
+export default FaceContainer;
