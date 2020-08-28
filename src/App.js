3f29d150d8c26a8fbe401f03e4dba0f0f9689bc7
Austin Pagotto <austinpagotto@gmail.com>
2020-08-28 13:47:37 -0400

    14 childBrushChart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
-import BrushChart from './Components/BrushChart'
+import BrushChart from "./Components/BrushChart";
+import BrushChartChild from './Components/BrushChartChild'
 import "./App.css";
 
-
 function App() {
-  const [data, setData] = useState([10, 25, 30, 40, 25, 60]);
+  const [data, setData] = useState(
+    Array.from({ length: 30 }).map(() => Math.round(Math.random() * 100))
+  );
 
   const onAddDataClick = () => {
-    setData([...data,Math.round(Math.random()*100)])
-  }
-
+    setData([...data, Math.round(Math.random() * 100)]);
+  };
 
   return (
     <React.Fragment>
       <h2>Sub-selections with d3-brush</h2>
-      <BrushChart data={data} />
+      <BrushChart data={data}>
+  {(selection)=><BrushChartChild data={data} selection = {selection}></BrushChartChild>}
+      </BrushChart>
       <br></br>
       <button onClick={onAddDataClick}>Add data</button>
     </React.Fragment>
--- src/Components/BrushChartChild.js
+++ src/Components/BrushChartChild.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import {
   select,
   scaleLinear,
@@ -7,37 +7,31 @@ import {
   curveCardinal,
   axisBottom,
   axisLeft,
-  brushX,
-  event,
 } from "d3";
 import useResizeObserver from "./useResizeObserver";
-import usePrevious from "./usePrevious";
 
-function BrushChart({ data,children }) {
+function BrushChart({ data, selection, id='myClipPath' }) {
   const svgRef = useRef();
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
-  const [selection, setSelection] = useState([0, 1.5]);
-  const previousSelection = usePrevious(selection);
 
   useEffect(() => {
     const svg = select(svgRef.current);
+    const content = svg.select('.content')
     const { width, height } =
       dimensions || wrapperRef.current.getBoundingClientRect();
 
-    const Xscale = scaleLinear()
-      .domain([0, data.length - 1])
-      .range([0, width]);
+    const Xscale = scaleLinear().domain(selection).range([0, width]);
     const Yscale = scaleLinear()
       .domain([0, max(data)])
-      .range([height, 0]);
+      .range([height-10, 10]);
 
     const lineGenerator = line()
       .x((d, index) => Xscale(index))
       .y((d) => Yscale(d))
       .curve(curveCardinal);
 
-    svg
+    content
       .selectAll(".myLine")
       .data([data])
       .join("path")
@@ -46,14 +40,18 @@ function BrushChart({ data,children }) {
       .attr("fill", "none")
       .attr("d", lineGenerator);
 
-    svg
+    content
       .selectAll(".myDot")
       .data(data)
       .join("circle")
       .attr("class", "myDot")
       .attr("stroke", "black")
-      .attr("r", (value,index)=>index >= selection[0] && index <= selection[1]?4:2)
-      .attr("fill", (value,index)=>index >= selection[0] && index <= selection[1]?'white':'black')
+      .attr("r", (value, index) =>
+        index >= selection[0] && index <= selection[1] ? 4 : 2
+      )
+      .attr("fill", (value, index) =>
+        index >= selection[0] && index <= selection[1] ? "white" : "black"
+      )
       .attr("cx", (value, index) => Xscale(index))
       .attr("cy", Yscale);
 
@@ -65,33 +63,21 @@ function BrushChart({ data,children }) {
       .call(xAxis);
     const yAxis = axisLeft(Yscale);
     svg.select(".y-axis").call(yAxis);
-
-    const brush = brushX()
-      .extent([
-        [0, 0],
-        [width, height],
-      ])
-      .on("start brush end", () => {
-        if (event.selection) {
-          const indexSelection = event.selection.map(Xscale.invert);
-          setSelection(indexSelection)
-        }
-      });
-
-    if (previousSelection === selection) {
-      svg.select(".brush").call(brush).call(brush.move, selection.map(Xscale));
-    }
-  }, [data, dimensions, selection,previousSelection]);
+  }, [data, dimensions, selection]);
   return (
     <React.Fragment>
       <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
         <svg ref={svgRef}>
+          <defs>
+            <clipPath id={id}>
+              <rect x="0" y="0" width="100%" height="100%" />
+            </clipPath>
+          </defs>
+          <g className="content" clipPath={`url(#${id})`} />
           <g className="x-axis" />
           <g className="y-axis" />
-          <g className="brush" />
         </svg>
       </div>
-      {children(selection)}
     </React.Fragment>
   );
 }
